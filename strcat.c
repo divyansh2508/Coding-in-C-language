6dd372497bd4411c3130228603183339cef5d789
Divyansh <86479436+divyansh2508@users.noreply.github.com>
2021-11-20 11:18:20 +0530

    Update strcat.c

--- strcat.c
+++ strcat.c
@@ -3,18 +3,19 @@
 #include <string.h>
 void main()
 {
+    //initialising two strings for concatenation//
     char str1[]="Hello ";
     char str2[]="World";
-    char str[100];
+    char str[100];   // taking another string for concatenation
     int i;
     for(i=0;i<strlen(str1);i++)
     {
-        str[i]=str1[i];
+        str[i]=str1[i];   //storing the elements of str1 in str
     }
     for(int j=0;j<strlen(str2);j++)
     {
-    	str[i]=str2[j];
-    	i++;
+    	str[i]=str2[j];   //storing the elments of str2 in str
+    	i++;   //increamenting the variable to jump to next cell
     }
-    printf("The concatenated string is: %s",str);
+    printf("The concatenated string is: %s",str);   //displaying the concatenated string
 }
