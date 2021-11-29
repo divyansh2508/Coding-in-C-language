3a5a06cf9b7fd5476af74eee0230feca5a623da7
Divyansh <86479436+divyansh2508@users.noreply.github.com>
2021-11-29 23:04:22 +0530

    Update A_and_C_of_circle.c

--- A_and_C_of_circle.c
+++ A_and_C_of_circle.c
@@ -1,23 +1,23 @@
-float area(int x);
-float circumference(int y);
 #include <stdio.h>
-void main()
+float area(int x); //Declaration of the function area
+float circumference(int y);  //Declaration of the function circumference
+void main() // Declaration of the main function
 {
     float r, area1, cf1;
     printf("Enter the radius of the circle: ");
     scanf("%f",&r);
-    area1=area(r);
-    cf1=circumference(r);
+    area1=area(r);  //Calling the function to calculate the area of the circle
+    cf1=circumference(r);  //Calling the function to calculate the circumference of the circle
     printf("The calculated area of the circle is: %f\n",area1);
     printf("The calculated circumference of the circle is: %f\n",cf1);
 }
-float area(int x)
+float area(int x)  //Definition of the area function
 {
     float A;
     A=3.14*x*x;
     return(A);
 }
-float circumference(int y)
+float circumference(int y)  //Definition of the circumference function
 {
     float C;
     C=2*3.14*y;
