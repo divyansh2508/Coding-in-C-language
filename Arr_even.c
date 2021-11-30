9513ab530596652f6bcc4d587f0e11a0f6acde88
Divyansh <86479436+divyansh2508@users.noreply.github.com>
2021-11-30 23:55:53 +0530

    Update Arr_even.c

--- Arr_even.c
+++ Arr_even.c
@@ -7,15 +7,15 @@ void main()
   int a[n];
   for(i=0;i<n;i++)
   {
-    printf("Enter element%d: ",i+1);
+    printf("Enter element%d: ",i+1); //inputing the elements in the array
     scanf("%d",&a[i]);
   }
   for(i=0;i<n;i++)
   {
-    if(a[i]%2==0)
+    if(a[i]%2==0) //checking each and every element if it is divisible by 2
     {
-       count+=1;
+       count+=1; //increasing the count if the condition is true
     }
   }
-  printf("The number of even elements in the entered array is %d.\n",count);
+  printf("The number of even elements in the entered array is %d.\n",count); //displaying the result according to entered array
 }
