680b3bc0a6dcd13da4b90956919dc7e2c529765f
Divyansh <86479436+divyansh2508@users.noreply.github.com>
2021-12-02 23:19:42 +0530

    Update Array_sum.c

--- Array_sum.c
+++ Array_sum.c
@@ -3,21 +3,21 @@ void main()
 {
   int x,y,i,j,sum=0;
   printf("Enter the order of the matrix [][]: ");
-  scanf("%d %d",&i,&j);
-  int a[i][j];
+  scanf("%d %d",&i,&j);  //initializing the order of the matrix
+  int a[i][j];  //declaring the size of the 2-D array
   for(x=0;x<i;x++)
   {
     for(y=0;y<j;y++)
     {
       printf("Enter Element ");
-      scanf("%d",&a[x][y]);
+      scanf("%d",&a[x][y]);  //entering the elements in the array
     }
   }
   for(x=0;x<i;x++)
   {
     for(y=0;y<j;y++)
     {  
-      sum+=a[x][y];
+      sum+=a[x][y];  //calculating the sum of the row elements of the matrix
     }
     printf("The sum of row%d is: %d\n",x,sum);
     sum=0;
@@ -26,7 +26,7 @@ void main()
   {
     for(x=0;x<i;x++)
     {  
-      sum+=a[x][y];
+      sum+=a[x][y];  //calculating the sum of the column elements of the matrix
     }
     printf("The sum of column%d is: %d\n",y,sum);
     sum=0;
