190ac6be01db68bb785b4e3d19b58b691a4dd19a
Divyansh <86479436+divyansh2508@users.noreply.github.com>
2021-12-06 23:39:20 +0530

    Update Circle(using func).c

--- Circle(using func).c
+++ Circle(using func).c
@@ -2,21 +2,21 @@ void main()
 {
    printf("Enter the radius of the circle");
    int r;
-   scanf("%d",&r);
-   A=area(r);
-   C=circum(r);
+   scanf("%d",&r);  //storing the value of the radius of the circle.
+   A=area(r);  //Call by value for area
+   C=circum(r);  //Call by value for circumference
    printf("The area of the circle is: %f",A);
    printf("The circumference of the circle is: %f",C);
 }
 float area(int x)
 {
    int a1;
-   a1=3.14*x*x;
-   return(a1);
+   a1=3.14*x*x;  //calculating the value of the area
+   return(a1);  //returning the value of calculated area
 }
 float circum(int y)
 {
     int c1;
-    c1=2*3.14*y;
-    return(c1);
+    c1=2*3.14*y;  //calculating the value of circumference
+    return(c1);  //returning the value of calculated circumference.
 }
